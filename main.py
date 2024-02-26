3b1d919521ac37c7e6610e9e360568006c781cb1
Qi Chai <140308209+QiChaiQiChai@users.noreply.github.com>
2024-02-26 17:39:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_ips_from_url(url):
 
 def get_location(ip):
     try:
-        response = requests.get(f"https://ip-api.com/json/{ip}")
+        response = requests.get(f"http://ip-api.com/json/{ip}")
         data = response.json()
         if data['status'] == 'success':
             return data['countryCode']
